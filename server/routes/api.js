fa0a5e450a210daef5034d7a922cfa972d42a13c
iosifnicolae2 <iosifnicolae2@gmail.com>
2018-05-03 23:12:06 +0300

    Improve code readability

--- server/routes/api.js
+++ server/routes/api.js
@@ -10,8 +10,10 @@ var QueueElementWaze = require('../model/queueElementWaze');
 var QueueElementAPs = require('../model/queueElementAPs');
 
 
-router.get('/test', function (req, res) {
-  res.json({ test: 'ok12' });
+router.get('/test', function(req, res) {
+    res.json({
+        test: 'ok12'
+    });
 });
 
 // Utils
@@ -20,114 +22,141 @@ function uniq_fast(a, field) {
     var out = [];
     var len = a.length;
     var j = 0;
-    for(var i = 0; i < len; i++) {
-         var item = a[i];
-         if(seen[item[field]] !== 1) {
-               seen[item[field]] = 1;
-               out[j++] = item;
-         }
+    for (var i = 0; i < len; i++) {
+        var item = a[i];
+        if (seen[item[field]] !== 1) {
+            seen[item[field]] = 1;
+            out[j++] = item;
+        }
     }
     return out;
 }
+module.exports = function(io) {
+    router.get('/queue/waze_clients', function(req, res) {
+        MINUTES_30 = 30 * 60 * 1000;
+        QueueElementWaze.find({
+            created_at: {
+                $gte: new Date(new Date().getTime() - MINUTES_30).toISOString()
+            }
+        }, function(err, queue_elements) {
+            var unique_queue_elements = uniq_fast(queue_elements, 'client_id');
+            var average_number_of_clients = unique_queue_elements.reduce((a, o, i, p) => a + o.number_of_clients / p.length, 0)
+            res.json({
+                err,
+                unique_queue_elements,
+                average_number_of_clients
+            });
+        });
+    });
 
-router.get('/queue/waze_clients', function (req, res) {
-  MINUTES_30 = 30*60*1000;
-  QueueElementWaze.find({
-    created_at: {
-      $gte: new Date(new Date().getTime() - MINUTES_30).toISOString()
-    }
-  }, function(err, queue_elements) {
-    var unique_queue_elements = uniq_fast(queue_elements, 'client_id');
-    var average_number_of_clients = unique_queue_elements.reduce((a, o, i, p) => a + o.number_of_clients / p.length, 0)
-    res.json({err, unique_queue_elements, average_number_of_clients});
-  });
-});
-
-router.post('/queue/waze_clients', function (req, res) {
-  // TODO(iosif) we need to validate user input
-  var queue_element = new QueueElementWaze({
-    client_id: req.body.client_id,
-    number_of_clients: req.body.number_of_clients,
-  });
-
-  queue_element.save(function (err, data) {
-        if (err) {
-          return res.json({ error: err, data: data , success: false});
-        }
-
-        res.json({error: false, success: true, data: data})
-      });
-
-});
-
-router.get('/queue/aps_clients', function (req, res) {
-  MINUTES_15 = 15*60*1000;
-  QueueElementAPs.find({
-    created_at: {
-      $gte: new Date(new Date().getTime() - MINUTES_15).toISOString()
-    }
-  }, function(err, queue_elements) {
-    var unique_queue_elements = uniq_fast(queue_elements, 'client_id');
-    var number_of_clients = unique_queue_elements.reduce((a, o, i, p) => a + o.in_the_queue, 0)
-    res.json({err, unique_queue_elements, number_of_clients});
-  });
-});
-
-router.post('/queue/aps_clients', function (req, res) {
-  // TODO(iosif) we need to validate user input
-  var queue_element = new QueueElementAPs({
-    client_id: req.body.client_id,
-    APs: req.body.APs,
-  });
-
-  queue_element.save(function (err, data) {
-        if (err) {
-          return res.json({ error: err, data: data , success: false});
-        }
-
-        res.json({error: false, success: true, data: data})
-      });
-
-});
-
-router.get('/orar', function (req, res) {
-    console.log('generate orar');
-    try {
-
-      var rows = [];
-      var writeStr = '';
-      //looping through all sheets
-      for (var i = 0; i < obj.length; i++)
-      {
-        var sheet = obj[i];
-        //loop through all rows in the sheet
-        for (var j = 0; j < sheet['data'].length; j++)
-        {
-          //add the row to the rows array
-          /*  if(sheet['data'][j]){
-              sheet['data'][j] = sheet['data'][j].filter(function(val) { return val !== null; }).join(", ");
-            }*/
-          rows.push(sheet['data'][j]);
-        }
-      }
-
-      //creates the csv string to write it to a file
-      for (var i = 0; i < rows.length; i++)
-      {
-        writeStr += rows[i].join('|') + '\n';
-      }
-
-      //writes to a file, but you will presumably send the csv as a
-      //response instead
-      fs.writeFile('./public/orar_csv/orar.csv', writeStr, function (err) {
-          if (err) {
-            return console.log(err);
-          }
-
-          res.json({ ping: true, err: err, data: rows });
+    router.post('/queue/waze_clients', function(req, res) {
+        // TODO(iosif) we need to validate user input
+        var queue_element = new QueueElementWaze({
+            client_id: req.body.client_id,
+            number_of_clients: req.body.number_of_clients,
         });
 
-      /*
+        queue_element.save(function(err, data) {
+            if (err) {
+                return res.json({
+                    error: err,
+                    data: data,
+                    success: false
+                });
+            }
+
+            io.emit('update_menu');
+            res.json({
+                error: false,
+                success: true,
+                data: data
+            })
+        });
+
+    });
+
+    router.get('/queue/aps_clients', function(req, res) {
+        MINUTES_15 = 15 * 60 * 1000;
+        QueueElementAPs.find({
+            created_at: {
+                $gte: new Date(new Date().getTime() - MINUTES_15).toISOString()
+            }
+        }, function(err, queue_elements) {
+            var unique_queue_elements = uniq_fast(queue_elements, 'client_id');
+            var number_of_clients = unique_queue_elements.reduce((a, o, i, p) => a + o.in_the_queue, 0)
+            res.json({
+                err,
+                unique_queue_elements,
+                number_of_clients
+            });
+        });
+    });
+
+    router.post('/queue/aps_clients', function(req, res) {
+        // TODO(iosif) we need to validate user input
+        var queue_element = new QueueElementAPs({
+            client_id: req.body.client_id,
+            APs: req.body.APs,
+        });
+
+        queue_element.save(function(err, data) {
+            if (err) {
+                return res.json({
+                    error: err,
+                    data: data,
+                    success: false
+                });
+            }
+
+            io.emit('update_menu');
+            res.json({
+                error: false,
+                success: true,
+                data: data
+            })
+        });
+
+    });
+
+    router.get('/orar', function(req, res) {
+        console.log('generate orar');
+        try {
+
+            var rows = [];
+            var writeStr = '';
+            //looping through all sheets
+            for (var i = 0; i < obj.length; i++) {
+                var sheet = obj[i];
+                //loop through all rows in the sheet
+                for (var j = 0; j < sheet['data'].length; j++) {
+                    //add the row to the rows array
+                    /*  if(sheet['data'][j]){
+                        sheet['data'][j] = sheet['data'][j].filter(function(val) { return val !== null; }).join(", ");
+                      }*/
+                    rows.push(sheet['data'][j]);
+                }
+            }
+
+            //creates the csv string to write it to a file
+            for (var i = 0; i < rows.length; i++) {
+                writeStr += rows[i].join('|') + '\n';
+            }
+
+            //writes to a file, but you will presumably send the csv as a
+            //response instead
+            fs.writeFile('./public/orar_csv/orar.csv', writeStr, function(err) {
+                if (err) {
+                    return console.log(err);
+                }
+
+                res.json({
+                    ping: true,
+                    err: err,
+                    data: rows
+                });
+            });
+
+            /*
 var workbook = XLSX.readFile('./public/orar/orar.xls');
 
 
@@ -144,11 +173,16 @@ for (z in worksheet) {
 });
 
 */
-    }catch (err) {
+        } catch (err) {
 
-      res.json({ ping: true, err: err });
-    }
+            res.json({
+                ping: true,
+                err: err
+            });
+        }
 
-  });
+    });
 
-module.exports = router;
+
+    return router;
+};
